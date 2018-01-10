abead5fb2087f2b7eb526b72bdfaa0b95269f769
DeepanshaAgarwal <35303183+DeepanshaAgarwal@users.noreply.github.com>
2018-01-10 21:48:59 +0530

    Add files via upload

---
+++ zero.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter number");
+scanf("%d",&n);
+if(n==0)
+{
+printf("Zero");
+}
+else if(n>0)
+{
+printf("Positive");
+}
+else{
+printf("Negative");
+}
+}

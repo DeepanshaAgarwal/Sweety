c9e9357fd77b84e3524ca3f23063d6d0bb20b3f6
DeepanshaAgarwal <35303183+DeepanshaAgarwal@users.noreply.github.com>
2018-01-12 18:48:38 +0530

    Add files via upload

---
+++ pallindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int n,a,b=0,c;
+	printf("enter no.");
+	scanf("%d",&n);
+	c=n;
+	while(n!=0)
+	{
+		a=n%10;
+		b=b*10+a;
+		n=n/10;
+		
+	}
+	if(c==b)
+		{printf("yes");
+		}
+		else
+		{
+			printf("no");
+		}
+}

8a68df34682cca3a758dddb0dcf10b959121c03d
DeepanshaAgarwal <35303183+DeepanshaAgarwal@users.noreply.github.com>
2018-01-15 22:04:46 +0530

    Add files via upload

---
+++ min.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	int n,k,j, a[10];
+	printf("enter no.");
+	scanf("%d",&n);
+	printf("enter array");
+	for(k=0;k<n;k++)
+	{
+		scanf("%d",&a[i]);
+	}
+	int min=a[0];
+	
+	for(j=1;j<n;j++)
+	{
+		if(a[j]<min)
+		{
+		min=a[j];
+		
+		}
+		
+	}
+	
+printf("%d",min);	
+	
+}
